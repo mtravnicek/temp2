7f08a8917879c9603f2f2ba250dbeb7488c2240b
javorka <javorka@users.noreply.github.com>
2016-11-24 09:21:48 +0100

    Exception translator (#21)

--- persistence/src/main/java/cz/muni/pa165/pneuservis/persistence/config/PersistenceConfiguration.java
+++ persistence/src/main/java/cz/muni/pa165/pneuservis/persistence/config/PersistenceConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -35,6 +36,11 @@ public class PersistenceConfiguration {
         factory.setDataSource(dataSource());
         factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
         return factory;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor postProcessor() {
+        return new PersistenceExceptionTranslationPostProcessor();
     }
 
     @Bean
