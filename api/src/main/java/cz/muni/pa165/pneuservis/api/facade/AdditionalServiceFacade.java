f03c38a790752b04af57dbcb9072c21c34f7716f
Peter Javorka <peter.javorka@artin.cz>
2016-11-25 14:39:10 +0100

    Javadoc - AdditionalServiceService + AdditionalServiceFacade

--- api/src/main/java/cz/muni/pa165/pneuservis/api/facade/AdditionalServiceFacade.java
+++ api/src/main/java/cz/muni/pa165/pneuservis/api/facade/AdditionalServiceFacade.java
@@ -8,9 +8,42 @@ import java.util.List;
  * Created by peter on 11/18/16.
  */
 public interface AdditionalServiceFacade {
+    /**
+     * Saves a given AdditionalServiceDTO. Use the returned instance for further operations
+     * as the save operation might have changed the entity instance completely.
+     *
+     * @param dto AdditionalServiceDTO to save
+     * @return the saved AdditionalServiceDTO
+     */
     AdditionalServiceDTO save(AdditionalServiceDTO dto);
+
+    /**
+     * Retrieves an AdditionalServiceDTO by its id.
+     *
+     * @param id must not be null.
+     * @return the AdditionalServiceDTO with the given id or null if none found
+     */
     AdditionalServiceDTO findOne(Long id);
+
+    /**
+     * Returns all instances of the AdditionalServiceDTO.
+     *
+     * @return all entities
+     */
     List<AdditionalServiceDTO> findAll();
+
+    /**
+     * Returns all instances of the AdditionalServiceDTO, which names contains given name
+     *
+     * @param name to search by
+     * @return all entities containing given name in name
+     */
     List<AdditionalServiceDTO> findByNameContaining(String name);
+
+    /**
+     * Deletes the AdditionalServiceDTO with the given id.
+     *
+     * @param id - must not be null.
+     */
     void delete(Long id);
 }
--- service/src/main/java/cz/muni/pa165/pneuservis/service/AdditionalServiceService.java
+++ service/src/main/java/cz/muni/pa165/pneuservis/service/AdditionalServiceService.java
@@ -9,9 +9,42 @@ import java.util.List;
  * Created by peter on 11/18/16.
  */
 public interface AdditionalServiceService {
+    /**
+     * Saves a given AdditionalService. Use the returned instance for further operations
+     * as the save operation might have changed the entity instance completely.
+     *
+     * @param service AdditionalService to save
+     * @return the saved AdditionalService
+     */
     AdditionalService save(AdditionalService service);
+
+    /**
+     * Retrieves an AdditionalService by its id.
+     *
+     * @param id must not be null.
+     * @return the AdditionalService with the given id or null if none found
+     */
     AdditionalService findOne(Long id);
+
+    /**
+     * Returns all instances of the AdditionalService.
+     *
+     * @return all entities
+     */
     List<AdditionalService> findAll();
+
+    /**
+     * Returns all instances of the AdditionalService, which names contains given name
+     *
+     * @param name to search by
+     * @return all entities containing given name in name
+     */
     List<AdditionalService> findByNameContaining(String name);
+
+    /**
+     * Deletes the AdditionalService with the given id.
+     *
+     * @param id - must not be null.
+     */
     void delete(Long id);
 }
